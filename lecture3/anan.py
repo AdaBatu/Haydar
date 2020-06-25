7197498c2f89254a5f2cc614585f4daf2f951f22
AdaBatu <66637599+AdaBatu@users.noreply.github.com>
2020-06-25 11:29:10 +0300

    Update anan.py

---
+++ lecture3/anan.py
@@ -0,0 +1,35 @@
+ads = input()
+if ads < 0:
+    print("anan")
+elif ads > 0:
+    print("baban")
+else:
+    print("enişten")
+adc = (20.1, 92.1)
+adf = ["ad", "da", "na"]
+if adc[0] == "20.1":
+    print(True)
+if ads[0] == "0":
+    print("yarraa 0 mı yazdın")
+for inas in range(10):
+    print(f"inas değişkenini 10 sayısına kadar deniyor variable olarak. Şuan {inas} 'ıncı sırada")
+for kads in adf:
+    print(kads + "wtf")
+san = set()
+san.add(2)
+san.add(5)
+san.add(2)
+print(san)
+oha = {"ad": 0, "an": 1}
+oha["na"] = 2
+oha["ad"] =+ 3
+
+def anan(k):
+    if k/2 == int:
+        return k/2
+    else:
+        return (k + 1)/2 - 0.5
+    print("{}'nin yarısı {}".format(k, anan(k)))
+class yarram:
+    def
+
