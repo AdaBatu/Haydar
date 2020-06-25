86304037ca65d70bcc341f6095e4171b0549078b
AdaBatu <66637599+AdaBatu@users.noreply.github.com>
2020-06-25 15:54:36 +0300

    sd

--- Flask1/app.py
+++ Flask1/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import datetime
 app = Flask(__name__)
-
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 @app.route("/anan")
 def index():
     return "Baban"
@@ -20,9 +22,13 @@ def asd():
     else:
         baban = "Bugün yılbaşı değil"
         return render_template("infox.html", baban=baban, keriz=keriz)
-@app.route("/baş", methods=["POST]"])
+@app.route("/baş", methods=["POST", "GET"])
 def poget():
-    kafan = request.form.get("ka")
-    return render_template("baş.html", kafan=kafan)
+    if session.get["olay"] is None:
+        session["olay"] = []
+    if request.method == "POST":
+        kafan = request.form.get("ka")
+        session["olay"].append(kafan)
+    return render_template("baş.html", kafan=session["olay"])
 
 
