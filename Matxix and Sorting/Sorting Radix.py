41738e79698bf1991820580abf3ebba968bab560
AdaBatu <66637599+AdaBatu@users.noreply.github.com>
2020-09-21 22:00:33 +0300

    not working but will

--- Matxix and Sorting/Sorting Radix.py
+++ Matxix and Sorting/Sorting Radix.py
@@ -1,11 +1,11 @@
 giriş = input("Istediğiniz işlemi yazınız:")
-liste = []
 sonuc = []
 anan = ""
-dictinor = {}
+list1 = []
 
 
 def sortfactor(liste):
+    dictinor = {}
     x0 = []
     x1 = []
     x2 = []
@@ -16,27 +16,75 @@ def sortfactor(liste):
     x7 = []
     x8 = []
     x9 = []
-    for x in liste:
-        for s in range(len(x)):
-            son = x[-(s + 1)]
-            dictinor[x] = son
-    liste.clear()
-    return print(dictinor)
+    bu = 1
+    while bu < (len(max(liste, key=len))):
+        for x in liste:
+            while bu < len(max(liste, key=len)):
+                son = x[-bu]
+                dictinor[x] = son
+                break
+            for i in dictinor:
+                c = dictinor.get(i)
+                if c == 0:
+                    x0.append(i)
+                if c == 1:
+                    x1.append(i)
+                if c == 2:
+                    x2.append(i)
+                if c == 3:
+                    x3.append(i)
+                if c == 4:
+                    x4.append(i)
+                if c == 5:
+                    x5.append(i)
+                if c == 6:
+                    x6.append(i)
+                if c == 7:
+                    x7.append(i)
+                if c == 8:
+                    x8.append(i)
+                if c == 9:
+                    x9.append(i)
+                dictinor.clear()
+                break
+            liste.clear()
+        if x0:
+            liste.append(x0)
+        if x1:
+            liste.append(x1)
+        if x2:
+            liste.append(x2)
+        if x3:
+            liste.append(x3)
+        if x4:
+            liste.append(x4)
+        if x5:
+            liste.append(x5)
+        if x6:
+            liste.append(x6)
+        if x7:
+            liste.append(x7)
+        if x8:
+            liste.append(x8)
+        if x9:
+            liste.append(x9)
+        bu += 1
+    return print(liste)
 
 
 if giriş == "sorting":
     while anan == int or "bitti":
         if anan != "bitti":
             anan = input("Sayınları giriniz.")
-            liste.append(anan)
+            list1.append(anan)
             print(anan + " Eklendi")
         else:
-            liste.remove("bitti")
-            print(liste)
+            list1.remove("bitti")
+            print(list1)
             break
     else:
         print("lütfen sayı giriniz")
-        liste.remove(anan)
-    sortfactor(liste)
+        list1.remove(anan)
+    sortfactor(list1)
 else:
     print("anan")
