f921f68b4e3018bbf3462d876f97e50e8a6b80ec
AdaBatu <66637599+AdaBatu@users.noreply.github.com>
2020-09-25 21:23:05 +0300

    More stable versionV 0.84

--- Matxix and Sorting/Arduino Control.py
+++ Matxix and Sorting/Arduino Control.py
@@ -4,7 +4,6 @@ from tkinter import *
 import tkinter
 
 board = pyfirmata.Arduino('COM3')
-
 it = pyfirmata.util.Iterator(board)
 it.start()
 pencere = tkinter.Tk()
@@ -12,27 +11,30 @@ pencere.title("sonuçlar:")
 liste34 = Listbox(pencere)
 triger = board.get_pin('d:10:o')
 echo = board.get_pin('d:8:i')
-time23 = 0
 orta = 1
-sarrah = input("Bitsin mi?")
 bacı = 1
-if sarrah != "evet":
-    while bacı:
-        triger.write(0)
-        time.sleep(0.002)
-        triger.write(1)
-        time.sleep(0.01)
-        triger.write(0)
-        time23 = 0
-        while echo.read(1):
-            time23 += 1
+def süre(echord):
+    time234 = 0
+    if echord.read:
+        while echord.read():
             time.sleep(0.001)
+            time234 += 1
         else:
-            break
-        distance = time23 * 0.034 / 2
-        liste34.insert(orta, distance)
-        orta += 1
-        time.sleep(0.02)
-else:
-    bacı = 0
+            return time234
+
+
+while bacı == 1:
+    triger.write(0)
+    time.sleep(0.002)
+    triger.write(1)
+    time.sleep(0.01)
+    triger.write(0)
+    time23 = 0
+    time23 = süre(echo)
+    distance = time23 * 0.034 / 2
+    liste34.insert(orta, distance)
+    print(distance)
+    orta += 1
+    time.sleep(0.02)
 liste34.mainloop()
+
