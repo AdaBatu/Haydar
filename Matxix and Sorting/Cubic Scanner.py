877f420b0d0055c1c31ea0d4d21ecb10c6d5f9b9
AdaBatu <66637599+AdaBatu@users.noreply.github.com>
2020-09-25 21:48:52 +0300

    More stable versionV 0.84

--- Matxix and Sorting/Cubic Scanner.py
+++ Matxix and Sorting/Cubic Scanner.py
@@ -1,16 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 distance1 = ()
+distance2 = ()
 tick_wait = ()
 rotor_speed = ()
 direction = ()
-waited=()
-
-    def konum_belirle(distance, hrotation, vrotation):
-        
-        return location =new:
-
-
+b=()
 while True:
     fig = plt.figure()
     ax = plt.axes(projection='3d', c='r', axis=0)
@@ -20,6 +15,4 @@ while True:
     ax.plot3D(xline, yline, zline, 'gray')
     ax.view_init(60, 35)
     plt.show()
-    if waited = True:
-        break
 
