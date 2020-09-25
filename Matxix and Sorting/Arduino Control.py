daf3ca6b9d4b8b0f3793dfcd64a9b97529768e54
AdaBatu <66637599+AdaBatu@users.noreply.github.com>
2020-09-25 21:38:29 +0300

    More stable versionV 0.84

--- Matxix and Sorting/Arduino Control.py
+++ Matxix and Sorting/Arduino Control.py
@@ -9,10 +9,8 @@ it.start()
 pencere = tkinter.Tk()
 pencere.title("sonuçlar:")
 liste34 = Listbox(pencere)
-triger = board.get_pin('d:10:o')
-echo = board.get_pin('d:8:i')
-orta = 1
-bacı = 1
+
+
 def süre(echord):
     time234 = 0
     if echord.read:
@@ -22,19 +20,30 @@ def süre(echord):
         else:
             return time234
 
+def summe():
+    triger = board.get_pin('d:10:o')
+    echo = board.get_pin('d:8:i')
+    orta = 1
+    bacı = 1
+    while bacı == 1:
+        triger.write(0)
+        time.sleep(0.002)
+        triger.write(1)
+        time.sleep(0.01)
+        triger.write(0)
+        time23 = 0
+        time23 = süre(echo)
+        distance = time23 * 0.034 / 2
+        liste34.insert(orta, distance)
+        print(distance)
+        orta += 1
+        time.sleep(0.02)
+    else:
+        return distance
 
-while bacı == 1:
-    triger.write(0)
-    time.sleep(0.002)
-    triger.write(1)
-    time.sleep(0.01)
-    triger.write(0)
-    time23 = 0
-    time23 = süre(echo)
-    distance = time23 * 0.034 / 2
-    liste34.insert(orta, distance)
-    print(distance)
-    orta += 1
-    time.sleep(0.02)
-liste34.mainloop()
+
+button = Button(pencere, text="start", command=summe)
+button.pack()
+pencere.geometry("640x840+200+200")
+pencere.mainloop()
 
