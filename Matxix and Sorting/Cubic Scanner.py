ec3e927d4be10069a1fc0727f904592c93a71e82
AdaBatu <66637599+AdaBatu@users.noreply.github.com>
2020-09-27 01:58:01 +0300

    %80 percent stable version V0.91

--- Matxix and Sorting/Cubic Scanner.py
+++ Matxix and Sorting/Cubic Scanner.py
@@ -3,10 +3,13 @@ import time
 import matplotlib.pyplot as plt
 import math
 from mpl_toolkits.mplot3d import Axes3D
-tick_wait = ()
-rotor_speed = ()
-direction = ()
-b = ()
+old_x = int
+old_y = int
+new_x = int
+new_y = int
+old_dist = int
+new_dist = int
+first_y = 0
 ser = serial.Serial('COM3', 9600)
 X_Grid = [0, 900]
 Y_Grid = [0, 900]
@@ -16,21 +19,27 @@ ax = plt.axes(projection='3d')
 ax.set_xlabel('X Axis')
 ax.set_ylabel('Y Axis')
 ax.set_zlabel('Z Axis')
-ax.set_title('Select Point')
+ax.set_title('3D Projection')
 while 1:
-    baban = 0
+    baban = 1
     for h in range(313):
         b = ser.readline().decode('ascii')
-        garr = int(''.join(filter(str.isdigit, b)))
-        if garr < 500:
-            print(b)
-            oma = garr
-        else:
-            oma = 0
-            garr = 0
-        o = math.sqrt((garr ** 2) + (oma ** 2) - (2 * oma * garr * math.cos(12)))
+        new_dist = int(''.join(filter(str.isdigit, b)))
+        if new_dist < 500:
+            if baban:
+                old_x = new_dist
+                old_dist = new_dist
+                baban = 0
+            else:
+                print(b)
+                o = math.sqrt((new_dist ** 2) + (old_dist ** 2) - (2 * old_dist * new_dist * math.cos(12)))
+                old_dist = new_dist
+                new_x, new_y = math.sqrt(o ** 2 - old_y ** 2 + 2 * old_y * new_y - new_y ** 2) + old_x, math.sqrt(o ** 2 - new_x ** 2 + 2 * new_x * old_x - old_x ** 2) + old_y
 
-        baban = oma
+                old_x, old_y = new_x, new_y
+        else:
+            baban = 1
+
         time.sleep(0.021 + ((1/3)/1000))
 
     zline = (0, 15, 1000)
