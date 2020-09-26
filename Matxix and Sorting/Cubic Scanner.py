648325e1e1429f7bb47f9b70c76a9c6ac6e1d3ed
AdaBatu <66637599+AdaBatu@users.noreply.github.com>
2020-09-26 16:14:20 +0300

    stable Arduino version V0.88 nearly

--- Matxix and Sorting/Cubic Scanner.py
+++ Matxix and Sorting/Cubic Scanner.py
@@ -1,11 +1,21 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import serial
+import time
 distance1 = ()
 distance2 = ()
 tick_wait = ()
 rotor_speed = ()
 direction = ()
 b=()
+ser = serial.Serial('COM3', 9600)
+while 1:
+    b = ser.readline().decode('ascii')
+    garr = int(''.join(filter(str.isdigit, b)))
+    if int(garr) < 500:
+        print(b)
+    time.sleep(0.001)
+ser.close()
+
 while True:
     fig = plt.figure()
     ax = plt.axes(projection='3d', c='r', axis=0)
--- Matxix and Sorting/Phyton get resoult.py
+++ Matxix and Sorting/Phyton get resoult.py
@@ -1,13 +1,2 @@
-import serial
-import time
 
-ser = serial.Serial('COM3', 9600)
-data = []                       # empty list to store the data
-while 1:
-    b = ser.readline().decode('ascii')
-    garr = int(''.join(filter(str.isdigit, b)))
-    if int(garr) < 500:
-        print(b)
-    time.sleep(0.001)
-ser.close()
 
