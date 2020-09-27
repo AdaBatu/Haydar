a89a23aa1025e24e705738faf175e30b2d89353f
AdaBatu <66637599+AdaBatu@users.noreply.github.com>
2020-09-27 14:00:51 +0300

    %100 percent stable version V0.99

--- Matxix and Sorting/Cubic Scanner.py
+++ Matxix and Sorting/Cubic Scanner.py
@@ -11,6 +11,9 @@ new_x = int
 new_y = int
 old_dist = int
 new_dist = int
+zline = []
+xline = []
+yline = []
 first_y = 0
 ser = serial.Serial('COM3', 9600)
 X_Grid = [0, 900]
@@ -31,14 +34,19 @@ def multiprocess(r, k, m, old_dist1):
     eq1 = Eq((sqrt(o ** 2 - x ** 2 + 2 * x * k - k ** 2) + m), y)
     eq2 = Eq((sqrt(r ** 2 - 202500 + 900 * x - x ** 2) + 450), y)
     sncu_dict = solve((eq1, eq2), x, y)
-    sncu_dict[x] = x
-    sncu_dict[y] = y
-    return x, y
+    return sncu_dict
+
+def addpoints(dicto):
+    xlin = dicto[0]
+    ylin = dicto[1]
+    zlin = dicto[2]
+    return xline.append(xlin), yline.append(ylin), zline.append(zlin)
 
 
 while 1:
     baban = 1
-    for h in range(313):
+    starttime = time.time()
+    for h in range(360):
         b = ser.readline().decode('ascii')
         new_dist = int(''.join(filter(str.isdigit, b)))
         if new_dist < 500:
@@ -46,31 +54,17 @@ while 1:
                 old_x = new_dist
                 old_dist = new_dist
                 baban = 0
+                xline.append(new_dist)
+                yline.append(first_y)
+                zline.append(0)
             else:
                 print(b)
-                if __name__ == '__main__':
-                    starttime = time.time()
-                    processes = []
-                    for i in range(0, 10):
-                        p = multiprocessing.Process(target=multiprocess, args=(i,))
-                        processes.append(p)
-                        p.start()
-                    for process in processes:
-                        process.join()
-                print(multiprocess(new_dist, old_x, old_y, old_dist))
-                print('That took {} seconds'.format(time.time() - starttime))
-                print(new_x, new_y)
-                old_x, old_y = new_x, new_y
-                old_dist = new_dist
         else:
             baban = 1
 
         time.sleep(0.021 + ((1/3)/1000))
-
-    zline = (0, 15, 1000)
-    xline = (0, 15, 1000)
-    yline = (0, 15, 1000)
-    ax.plot3D(xline, yline, zline, 'gray')
+    print('That took {} seconds'.format(time.time() - starttime))
+    ax.scatter(xline, yline, zline, 'gray')
     ax.view_init(60, 35)
     plt.show()
-    ser.close()
+ser.close()
