91afb6de73300ea6320adf6f4dd59cf9810eee66
AdaBatu <66637599+AdaBatu@users.noreply.github.com>
2020-09-27 12:35:42 +0300

    %30 percent stable version V0.94.6

--- Matxix and Sorting/Cubic Scanner.py
+++ Matxix and Sorting/Cubic Scanner.py
@@ -3,13 +3,14 @@ import time
 import matplotlib.pyplot as plt
 import math
 from sympy import symbols, Eq, solve, sqrt, cos
+import multiprocessing
 from mpl_toolkits.mplot3d import Axes3D
-k = int
-m = int
-x = int
-y = int
+old_x = int
+old_y = int
+new_x = int
+new_y = int
 old_dist = int
-r = int
+new_dist = int
 first_y = 0
 ser = serial.Serial('COM3', 9600)
 X_Grid = [0, 900]
@@ -21,27 +22,46 @@ ax.set_xlabel('X Axis')
 ax.set_ylabel('Y Axis')
 ax.set_zlabel('Z Axis')
 ax.set_title('3D Projection')
+
+
+def multiprocess(r, k, m, old_dist1):
+    o = math.sqrt((r ** 2) + (old_dist1 ** 2) - (2 * old_dist1 * r * cos(12)))
+    x, y, k, m, o, r = symbols('x y k m o r')
+    multiprocessing.pool
+    eq1 = Eq((sqrt(o ** 2 - x ** 2 + 2 * x * k - k ** 2) + m), y)
+    eq2 = Eq((sqrt(r ** 2 - 202500 + 900 * x - x ** 2) + 450), y)
+    sncu_dict = solve((eq1, eq2), x, y)
+    sncu_dict[x] = x
+    sncu_dict[y] = y
+    return x, y
+
+
 while 1:
     baban = 1
     for h in range(313):
         b = ser.readline().decode('ascii')
-        r = int(''.join(filter(str.isdigit, b)))
-        if r < 500:
+        new_dist = int(''.join(filter(str.isdigit, b)))
+        if new_dist < 500:
             if baban:
-                k = r
-                old_dist = r
+                old_x = new_dist
+                old_dist = new_dist
                 baban = 0
             else:
                 print(b)
-                o = math.sqrt((r ** 2) + (old_dist ** 2) - (2 * old_dist * r * cos(12)))
-                x, y, k, m, o, r = symbols('x y k m o r')
-                eq1 = Eq((sqrt(o**2 - x**2 + 2*x*k - k**2) + m), y)
-                eq2 = Eq((sqrt(r**2 - 202500 + 900*x - x**2) + 450), y)
-                sncu_dict = solve((eq1, eq2), x, y)
-                print(sncu_dict)
-                print(x, y)
-                k, m = x, y
-                old_dist = r
+                if __name__ == '__main__':
+                    starttime = time.time()
+                    processes = []
+                    for i in range(0, 10):
+                        p = multiprocessing.Process(target=multiprocess, args=(i,))
+                        processes.append(p)
+                        p.start()
+                    for process in processes:
+                        process.join()
+                print(multiprocess(new_dist, old_x, old_y, old_dist))
+                print('That took {} seconds'.format(time.time() - starttime))
+                print(new_x, new_y)
+                old_x, old_y = new_x, new_y
+                old_dist = new_dist
         else:
             baban = 1
 
