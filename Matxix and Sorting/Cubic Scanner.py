ff0a91b555c47c195388ae2ebcb59c27cb02a48d
AdaBatu <66637599+AdaBatu@users.noreply.github.com>
2020-09-28 22:10:43 +0300

    %90 percent stable version V0.999

--- Matxix and Sorting/Cubic Scanner.py
+++ Matxix and Sorting/Cubic Scanner.py
@@ -11,14 +11,19 @@ new_x = int
 new_y = int
 old_dist = int
 new_dist = int
+ilerleyis = 12
+multiplyer1 = int
+multiplyer2 = int
 zline = []
 xline = []
 yline = []
 first_y = 0
+new_z = 450
 ser = serial.Serial('COM3', 9600)
 X_Grid = [0, 900]
 Y_Grid = [0, 900]
 Z_Grid = [0, 900]
+phase = 0
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 ax.set_xlabel('X Axis')
@@ -27,8 +32,8 @@ ax.set_zlabel('Z Axis')
 ax.set_title('3D Projection')
 
 
-def multiprocess2(r, k, m, old_dist1):
-    o = math.sqrt((r ** 2) + (old_dist1 ** 2) - (2 * old_dist1 * r * cos(12)))
+def multiprocess2(r, k, m, old_dist1, mult1, multi2):
+    o = math.sqrt((r ** 2) + (old_dist1 ** 2) - (2 * old_dist1 * r * cos(ilerleyis)))
     x = (r ** 2 - o ** 2 - m ** 2 + k ** 2) / (2 * k - 900)
     y = math.sqrt(o ** 2 - x ** 2 + 2 * x * k - k ** 2 + m ** 2)
     z = 450
@@ -46,6 +51,8 @@ while 1:
     baban = 1
     starttime = time.time()
     for h in range(360):
+        if phase > 348:
+            phase -= 360
         b = ser.readline().decode('ascii')
         new_dist = int(''.join(filter(str.isdigit, b)))
         if new_dist < 500:
@@ -59,13 +66,38 @@ while 1:
                 zline.append(0)
             else:
                 print(b)
-                dis_dict = multiprocess2(new_dist, old_x, old_y, old_dist)
+                if phase < 90:
+                    multiplyer1 = 1
+                    multiplyer2 = 1
+                if phase < 180 or phase > 90:
+                    multiplyer1 = -1
+                    multiplyer2 = 1
+                if phase > 180 or phase < 270:
+                    multiplyer1 = -1
+                    multiplyer2 = -1
+                if phase > 270:
+                    multiplyer1 = 1
+                    multiplyer2 = -1
+                dis_dict = multiprocess2(new_dist, old_x, old_y, old_dist, multiplyer1, multiplyer2)
+                if phase == 90:
+                    new_x = 0
+                    new_y = new_dist
+                    dis_dict = [new_x, new_y, new_z]
+                if phase == 180:
+                    new_y = 0
+                    new_x = new_dist
+                    dis_dict = [new_x, new_y, new_z]
+                if phase == 270:
+                    new_x = 0
+                    new_y = -new_dist
+                    dis_dict = [new_x, new_y, new_z]
                 addpoints(dis_dict)
                 old_x = dis_dict[0]
                 old_y = dis_dict[1]
+                dis_dict = []
         else:
             baban = 1
-
+        phase += ilerleyis
         time.sleep(0.021 + ((1/3)/1000))
     print('That took {} seconds'.format(time.time() - starttime))
     ax.scatter(xline, yline, zline, 'gray', cmap='brg')
