70af6b28b7444e7305c0b78a168ff83bd6dd7314
AdaBatu <66637599+AdaBatu@users.noreply.github.com>
2020-09-29 16:20:42 +0300

    %90 percent stable version V0.999 problem division by zero

--- Matxix and Sorting/Cubic Scanner.py
+++ Matxix and Sorting/Cubic Scanner.py
@@ -5,25 +5,25 @@ import matplotlib.cm as cm
 import matplotlib as mpl
 import math
 from mpl_toolkits.mplot3d import Axes3D
-old_x = int
-old_y = int
-new_x = int
-new_y = int
-old_dist = int
-new_dist = int
+old_x = 0
+old_y = 0
+new_x = 0
+new_y = 0
+old_dist = 0
+new_dist = 0
 ilerleyis = 12
-multiplyer1 = int
-multiplyer2 = int
+multiplyer1 = 0
+multiplyer2 = 0
 zline = []
 xline = []
 yline = []
 colores = []
-first_x = 450
+first_y = 450
 new_z = 450
 ser = serial.Serial('COM3', 9600)
 X_Grid = [0, 900]
 Y_Grid = [0, 900]
-Z_Grid = [0, 000]
+Z_Grid = [0, 900]
 phase = 0
 fig = plt.figure()
 ax = plt.axes(projection='3d')
@@ -35,7 +35,7 @@ ax.set_title('3D Projection')
 
 def multiprocess2(r, k, m, old_dist1, multi1, multi2):
     o = math.sqrt((r ** 2) + (old_dist1 ** 2) - (2 * old_dist1 * r * math.cos(ilerleyis)))
-    x = (r ** 2 - o ** 2 - m ** 2 + k ** 2) / (2 * k - 900)
+    x = (r ** 2 - o ** 2 - multi1 * m ** 2 + multi2 * k ** 2) / (2 * k - 900)
     y = math.sqrt(o ** 2 - x ** 2 + 2 * x * k - k ** 2 + m ** 2)
     z = 450
     sncu_dict = [x, y, z]
@@ -46,60 +46,64 @@ def addpoints(dicto, dist):
     ylin = dicto[1]
     zlin = dicto[2]
     bole = dist / 450
-    norm = mpl.colors.Normalize(vmin=3, vmax=450)
-    cmap = cm.brg
-    m = cm.ScalarMappable(norm=norm, cmap=cmap)
-    return xline.append(xlin), yline.append(ylin), zline.append(zlin), colores.append(m.to_rgba(bole))
+    norm = mpl.colors.Normalize(vmin=0, vmax=1)
+    cmap = cm.get_cmap(cm.brg)
+    rgb = cmap(norm(abs(bole)))[:3]
+    color = mpl.colors.rgb2hex(rgb)
+    return xline.append(xlin), yline.append(ylin), zline.append(zlin), colores.append(color)
 
 
 while 1:
     baban = 1
     starttime = time.time()
-    for h in range(360):
+    for h in range(4):
         if phase > 348:
             phase -= 360
         b = ser.readline().decode('ascii')
         new_dist = int(''.join(filter(str.isdigit, b)))
         if new_dist < 500:
             if baban:
-                old_y = (450 + new_dist)
-                old_x = first_x
+                old_x = (450 + new_dist)
+                old_y = first_y
                 old_dist = new_dist
                 baban = 0
-                xline.append(first_x)
-                yline.append(new_dist)
-                zline.append(0)
+                yline.append(first_y)
+                xline.append(new_dist + 450)
+                zline.append(new_z)
             else:
                 print(b)
-                if phase < 90:
-                    multiplyer1 = 1
-                    multiplyer2 = 1
-                if phase < 180 or phase > 90:
-                    multiplyer1 = -1
-                    multiplyer2 = 1
-                if phase > 180 or phase < 270:
-                    multiplyer1 = -1
-                    multiplyer2 = -1
-                if phase > 270:
-                    multiplyer1 = 1
-                    multiplyer2 = -1
-                dis_dict = multiprocess2(new_dist, old_x, old_y, old_dist, multiplyer1, multiplyer2)
-                print(new_dist, old_x, old_y, old_dist, dis_dict)
-                if phase == 90:
-                    new_x = 0
-                    new_y = new_dist
-                    dis_dict = [new_x, new_y, new_z]
-                if phase == 180:
-                    new_y = 0
-                    new_x = new_dist
-                    dis_dict = [new_x, new_y, new_z]
-                if phase == 270:
-                    new_x = 0
-                    new_y = -new_dist
-                    dis_dict = [new_x, new_y, new_z]
+                if phase != 90 or 180 or 270:
+                    if phase < 90:
+                        multiplyer1 = 1
+                        multiplyer2 = 1
+                    if 180 > phase > 90:
+                        multiplyer1 = -1
+                        multiplyer2 = 1
+                    if 180 < phase < 270:
+                        multiplyer1 = -1
+                        multiplyer2 = -1
+                    if phase > 270:
+                        multiplyer1 = 1
+                        multiplyer2 = -1
+                    dis_dict = multiprocess2(new_dist, old_x, old_y, old_dist, multiplyer1, multiplyer2)
+                    print(new_dist, old_x, old_y, old_dist, dis_dict)
+                else:
+                    if phase == 90:
+                        new_x = 450
+                        new_y = new_dist + 450
+                        dis_dict = [new_x, new_y, new_z]
+                    if phase == 180:
+                        new_y = 450
+                        new_x = new_dist + 450
+                        dis_dict = [new_x, new_y, new_z]
+                    if phase == 270:
+                        new_x = 450
+                        new_y = -new_dist + 450
+                        dis_dict = [new_x, new_y, new_z]
                 addpoints(dis_dict, new_dist)
                 old_x = dis_dict[0]
                 old_y = dis_dict[1]
+                old_dist = new_dist
                 dis_dict.clear()
         else:
             baban = 1
